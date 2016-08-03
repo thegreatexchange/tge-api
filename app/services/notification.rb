ac1cbbf57a1159582ec46624968797f70409849c
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-08-03 09:03:00 -0400

    Base Service

---
+++ app/services/notification.rb
@@ -0,0 +1,3 @@
+class NotificationService < BaseService
+
+end
---
+++ app/services/response.rb
@@ -0,0 +1,16 @@
+class ServiceResponse
+  attr_accessor :data, :errors
+
+  def has_errors?
+    !errors.empty?
+  end
+
+  def data
+    @data ||= {}
+  end
+
+  def errors
+    @errors ||= {}
+  end
+
+end
