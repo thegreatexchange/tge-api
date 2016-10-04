9b1ca1c6dc075e985c5d750ebb710ac9bede6761
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-10-03 21:23:25 -0400

    fix render format

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -5,7 +5,7 @@ class ClientsController < ApplicationController
     redis        = Redis.new(url: ENV['REDIS_URL'])
     revision     = params[:revision] || ENV['CLIENT_REVISION'] || redis.get("tge-admin-client:index:current")
 
-    render text: redis.get("tge-admin-client:index:#{revision}"), layout: false
+    render html: redis.get("tge-admin-client:index:#{revision}"), layout: false
   end
 
 end
