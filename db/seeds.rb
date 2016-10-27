8fdbb93fee393224c12c034baa92e64f68b4e251
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-10-27 08:40:29 -0400

    Seed events

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,7 @@ Rake.application['generate:authorizations:seed'].invoke
 Rake.application['generate:locations:seed'].invoke
 Rake.application['generate:schools:seed'].invoke
 Rake.application['generate:ministries:seed'].invoke
-Rake.application['generate:users:super'].invoke if Rails.env.development?
+if Rails.env.development?
+  Rake.application['generate:users:super'].invoke
+  Rake.application['generate:events:seed'].invoke
+end
