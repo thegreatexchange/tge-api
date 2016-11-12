ce9bbf4f9ee878c976fe7f5d2fb186e0894457e3
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-11-12 15:22:54 -0500

    fix required libs

--- config/application.rb
+++ config/application.rb
@@ -3,14 +3,14 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-# require "action_view/railtie"
-# require "action_cable/engine"
+require "action_view/railtie"
+require "action_cable/engine"
 # require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require "rails/test_unit/railtie"
 
 require_relative '../app/serializer_adapters/base'
 
