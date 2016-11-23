016ad05fbdbe16eeeb0b4a746d6da617118fd04a
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-11-23 08:45:29 -0500

    update seeds for qa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -4,7 +4,6 @@ Rails.application.configure do
   # Code is not reloaded between requests.
   config.cache_classes = true
 
-  config.logger = Logger.new(STDOUT)
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
@@ -38,6 +37,7 @@ Rails.application.configure do
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
   config.log_level = ENV['LOG_LEVEL'] || 'debug'
+  config.logger = Logger.new(STDOUT)
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,5 @@ Rake.application['generate:authorizations:seed'].invoke
 Rake.application['generate:locations:seed'].invoke
 Rake.application['generate:schools:seed'].invoke
 Rake.application['generate:ministries:seed'].invoke
-if Rails.env.development?
-  Rake.application['generate:users:super'].invoke
-  Rake.application['generate:events:seed'].invoke
-end
+Rake.application['generate:users:super'].invoke
+Rake.application['generate:events:seed'].invoke
