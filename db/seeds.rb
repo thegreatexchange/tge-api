5fa1fafd0236bb9e1f6754f8393cd9fc8a9e9de0
Brian David Wetzel <briandavidwetzel@gmail.com>
2016-12-15 00:09:21 -0500

    refactor user authorization

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#TODO: add new env for production to allow QA resets.
 Rake.application['generate:authorizations:seed'].invoke
 Rake.application['generate:locations:seed'].invoke
 Rake.application['generate:schools:seed'].invoke
