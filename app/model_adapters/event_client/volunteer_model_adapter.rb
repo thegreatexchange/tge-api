bad0f3fe1170d422d1ad515d569687bf862ec630
Brian David Wetzel <briandavidwetzel@gmail.com>
2017-04-01 10:57:06 -0400

    Add MC registration to volunteer creation

--- app/model_adapters/event_client/volunteer_model_adapter.rb
+++ app/model_adapters/event_client/volunteer_model_adapter.rb
@@ -3,25 +3,42 @@ require 'rapid_api/model_adapters'
 class EventClient::VolunteerModelAdapter < RapidApi::ModelAdapters::ActiveRecordAdapter
 
   def create(params, scope={})
-    klass.transaction do
-      event_id = params.delete(:event_id)
-      organization_id = params.delete(:organization_id)
-      comments = params.delete(:comments)
-      create_params = params.merge scope
-      member = klass.create create_params
-      if organization_id.present? && !member.errors.any?
-        organization = Organization.find organization_id
+    event_id = params.delete(:event_id)
+    organization_id = params.delete(:organization_id)
+    comments = params.delete(:comments)
+    create_params = params.merge scope
+    member = klass.new create_params
 
-        organization.organization_memberships.create person: member
+    begin
+      klass.transaction do
+        begin
+          member.save
+          if organization_id.present? && !member.errors.any?
+            organization = Organization.find organization_id
+
+            organizationMembership = organization.organization_memberships.new person: member
+            if organizationMembership.valid?
+              if MailchimpHelper.add_member_to_mailchimp_list_for(organizationMembership)
+                organizationMembership.save
+              else
+                raise "Failure adding member to mailchimp"
+              end
+            end
+          end
+
+          if event_id.present? && !member.errors.any?
+            event = Event.find event_id
+
+            EventRegistration.create event: event, person: member, comments: comments
+          end
+        rescue => e
+          member.errors.add :error, e.message
+          raise ActiveRecord::Rollback
+        end
       end
-
-      if event_id.present? && !member.errors.any?
-        event = Event.find event_id
-
-        EventRegistration.create event: event, person: member, comments: comments
-      end
-      _query_result_for_member member
+    rescue => e
     end
+    _query_result_for_member member
   end
 
 end
---
+++ app/model_adapters/mailchimp_helper.rb
@@ -0,0 +1,85 @@
+class MailchimpHelper
+
+  def self.add_member_to_mailchimp_list_for(organizationMembership)
+    organization_id = organizationMembership.organization_id
+    person_id       = organizationMembership.person_id
+    list_ids        = OrganizationList.where(organization_id: organization_id).pluck(:mailchimp_list_id)
+    return true if list_ids.empty?
+
+    #TODO: Throwing this over the fence and assuming it works. Need more robust handling.
+    list_ids.each do |list_id|
+      MailchimpService.subscribe!(list_id: list_id, person_id: person_id)
+    end
+
+    return true
+  end
+
+  def self.remove_member_from_mailchimp_list_for(organizationMembership)
+    organization_id = organizationMembership.organization_id
+    person_id       = organizationMembership.person_id
+    list_ids        = OrganizationList.where(organization_id: organization_id).pluck(:mailchimp_list_id)
+    return true if list_ids.empty?
+
+    filtered_list_ids = filter_out_duplicate_lists(list_ids, organization_id, person_id)
+    #TODO: Throwing this over the fence and assuming it works. Need more robust handling.
+    filtered_list_ids.each do |list_id|
+      MailchimpService.unsubscribe!(list_id: list_id, person_id: person_id)
+    end
+
+    return true
+  end
+
+  #NOTE: Some people may belong to organizations that are part of the same mailchimp list.
+  # We don't want to remove someone from lists if they belong to it via another organization.
+  def self.filter_out_duplicate_lists(list_ids, organization_id, person_id)
+    q = Arel.sql %Q{
+      SELECT l.mailchimp_list_id FROM organization_memberships m
+      JOIN organization_lists l ON m.organization_id = l.organization_id
+      WHERE l.organization_id != #{organization_id}
+      AND m.person_id = #{person_id}
+      GROUP BY l.mailchimp_list_id
+    }
+    filtered_out_list_ids = ActiveRecord::Base.connection.execute(q).to_a.map {|r| r['mailchimp_list_id']}
+    list_ids - filtered_out_list_ids
+  end
+
+  def self.add_members_to_mailchimp_list_for(organizationList)
+    organization_id = organizationList.organization_id
+    list_id         = organizationList.mailchimp_list_id
+    person_ids = OrganizationMembership.where(organization_id: organization_id).pluck(:person_id)
+    return true if person_ids.empty?
+
+    #TODO: Throwing this over the fence and assuming it works. Need more robust handling.
+    MailchimpService.batch_subscribe!(list_id: list_id, person_ids: person_ids)
+
+    return true
+  end
+
+  def self.remove_members_from_mailchimp_list_for(organizationList)
+    organization_id = organizationList.organization_id
+    list_id         = organizationList.mailchimp_list_id
+    person_ids = OrganizationMembership.where(organization_id: organization_id).pluck(:person_id)
+    return true if person_ids.empty?
+
+    filtered_person_ids = filter_out_duplicate_people(list_id, organization_id, person_ids)
+
+    #TODO: Throwing this over the fence and assuming it works. Need more robust handling.
+    MailchimpService.batch_unsubscribe!(list_id: list_id, person_ids: filtered_person_ids)
+
+    return true
+  end
+
+  #NOTE: Some people may belong to organizations that are part of the same mailchimp list.
+  # We don't want to remove those people from the list.
+  def self.filter_out_duplicate_people(list_id, organization_id, person_ids)
+    q = Arel.sql %Q{
+      SELECT m.person_id FROM organization_memberships m
+      JOIN organization_lists l ON m.organization_id = l.organization_id
+      WHERE l.mailchimp_list_id = '#{list_id}' AND l.organization_id != #{organization_id}
+      GROUP BY m.person_id
+    }
+    filtered_out_person_ids = ActiveRecord::Base.connection.execute(q).to_a.map {|r| r['person_id']}
+    person_ids - filtered_out_person_ids
+  end
+
+end
